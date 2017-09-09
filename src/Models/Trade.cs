f79bdbfd16889afb3a8cfa4e50ecf4e109180f8c
Renata Oliveira <renatabels@gmail.com>
2017-09-09 19:44:18 -0300

    Changing the error message for validations

--- src/Models/Trade.cs
+++ src/Models/Trade.cs
@@ -11,25 +11,25 @@ namespace Trading.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Código é obrigatório.")]
         [Remote(action: "VerifyCode", controller: "Trade")]
         public string Code { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Tipo de Mercadoria é obrigatório.")]
         public string Type { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Nome da Mercadoria é obrigatório.")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "A Quantidade de Mercadoria é obrigatória.")]
         [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Preço da mercadoria é obrigatório.")]
         [Range(0.01, 99999999999999.99)]
         public decimal Price { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Tipo do Negócio é obrigatório.")]
         public BusinessType BusinessType { get; set; }
     }
 

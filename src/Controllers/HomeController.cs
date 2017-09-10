a0a3817c865b401c939e14d0a04a33c134d66f31
Renata Oliveira <renatabels@gmail.com>
2017-09-10 00:07:45 -0300

    Adding dynamic content from GitHub

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +14,20 @@ namespace Trading.Controllers
     [Route("")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var readmeUri = "https://raw.githubusercontent.com/rnataoliveira/test-web-developer/master/README.md";
+
+            //fazer uma request
+            var client = new HttpClient();
+            var content = await client.GetStringAsync(new Uri(readmeUri));
+            var result = CommonMark.CommonMarkConverter.Convert(content);
+            
+            //utlizando um http client
+
+            //retrnaro conteudo da request para a view
+
+            return View(new HtmlString(result));
         }
     }
 }
